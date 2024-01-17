ed098142dc95a4fe4a26eaefe17fe689a9835068
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-01-17 18:36:44 +0100

    2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: youneshanafi <youneshanafi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 12:59:09 by youneshanaf       #+#    #+#             */
-/*   Updated: 2024/01/15 14:33:13 by youneshanaf      ###   ########.fr       */
+/*   Updated: 2024/01/17 18:36:29 by youneshanaf      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ int	main(int argc, *argv, char **env)
 	analyse_output(argv[argc - 1], &pipex);
 	//4: analyse cmd;
 	analyse_cmd(argc, argv, &pipex);
+	
 }

4b59cd8b0a92af74910fe142ca38e9e36b0fc769
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-03-18 13:49:59 +0100

    1

---
+++ pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 09:58:00 by yohanafi          #+#    #+#             */
+/*   Updated: 2024/03/18 12:18:01 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+
+# define EM "ERROR INPUT\nTHE CORRECT INPUT:\n<./pipex files1 cmd1 cmd2 outfile>"
+
+typedef struct	s_pipex
+{
+	int		fd1;
+	int		fd2;
+	char	*cmd;
+}				t_pipex;
+
+#endif

fc84fa20734c062c1e8718ff0a16768528988c4f
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-03-21 14:22:09 +0100

    1

--- init_pipex.c
+++ init_pipex.c
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   init_pipex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/18 09:58:00 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/03/21 14:05:13 by yohanafi         ###   ########.fr       */
+/*   Created: 2024/03/21 11:44:34 by yohanafi          #+#    #+#             */
+/*   Updated: 2024/03/21 11:44:36 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-
-# include <stdio.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-
-# define EM "ERROR INPUT\nTHE CORRECT INPUT:\n<./pipex files1 cmd1 cmd2 outfile>"
-
-typedef struct	s_pipex
-{
-	int		pipe[2];
-	char	*path;
-	char	*cmd;
-}				t_pipex;
-
-
-char	**ft_split(char const *s, char c);
-#endif
--- pipex.c
+++ pipex.c
@@ -6,26 +6,44 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 09:58:06 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/03/20 16:22:04 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/03/21 14:07:35 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+// 1. get the env
+// 2. recup the path
+// 3. split
+// 4. acces the argv
+// 5. exev
 
-int	main(int argc, char **argv, char *env)
+int	ft_strncmp(const char *s1, char *s2, size_t n)
 {
-	t_pipex	*pipe;
-	
-	if ( 0 > open(argv[1], O_RDONLY) || 0 > open(argv[4]))
-		return (-1);
-	pipe->fd1 = open(argv[1], O_RDONLY);
-	pipe->fd2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC)
+	size_t	i;
 
-	if (argc == 5)
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n && (s1[i] || s2[i]))
 	{
-		init_pipex()
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
 	}
-	else
-		perror(EM);
 	return (0);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_pipex	pipex;
+
+	while (*env)
+	{
+		if (!ft_strncmp(*env, "PATH", 4))
+			pipex.path = *env + 5;
+		env++;
+	}
+	pipex.cmd = *ft_split(pipex.path, ':');
+	printf("%c",pipex.cmd);
+	return(0);
 }

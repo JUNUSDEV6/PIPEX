75ce5064e801e08adce4d1780df3d1c7ca923373
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-03-28 10:52:47 +0100

    1

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 09:58:06 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/03/26 11:17:25 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/03/28 10:50:12 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	main(int argc, char **argv, char **env)
 	t_pipex	pipex;
 	int		i = 0;
 	char	*full_paths;
+	char	**cmd = ft_split(argv[1], ' ');
 	while (*env)
 	{
 		if (!ft_strncmp(*env, "PATH", 4))
@@ -37,37 +38,30 @@ int	main(int argc, char **argv, char **env)
 		printf("Aucun chemin trouvé dans la variable PATH.\n");
 		return (1);
 	}
-	int pid = fork();
-	if (!pid) {
-		while (pipex.path[i])
+	while (pipex.path[i])
+	{
+		full_paths = malloc(ft_strlen(pipex.path[i]) + ft_strlen(argv[1]) + 2);
+		if (!full_paths)
 		{
-			full_paths = malloc(ft_strlen(pipex.path[i]) + ft_strlen(argv[1]) + 2);
-			if (!full_paths)
-			{
-				perror("ERROR MALLOC");
-				exit(EXIT_FAILURE);
-			}
-			//printf("%s\n", pipex.path[i]);
-			ft_strcpy(full_paths, pipex.path[i]);
-			ft_strcat(full_paths, "/");
-			ft_strcat(full_paths, argv[1]);
-			if (access(full_paths, X_OK) == 0)
-			{
-				//printf("yo\n");
-				printf("command find : %s\n", full_paths);
-				execve(full_paths, &argv[1], env);
-				perror("ERROR");
-				free(full_paths);
-				exit(EXIT_FAILURE);
-			}
-			//printf("pas bonne commande");
-			free(full_paths);
-			i++;
+			perror("ERROR MALLOC");
+			exit(EXIT_FAILURE);
 		}
-	}
-	else if (pid) {
-		waitpid()
-		printf("Le child a fini.\n");
+		//printf("%s\n", pipex.path[i]);
+		ft_strcpy(full_paths, pipex.path[i]);
+		ft_strcat(full_paths, "/");
+		ft_strcat(full_paths, cmd[0]);
+		if (access(full_paths, X_OK) == 0)
+		{
+			//printf("yo\n");
+			printf("command find : %s\n", full_paths);
+			execve(full_paths, cmd, env);
+			perror("ERROR");
+			free(full_paths);
+			exit(EXIT_FAILURE);
+		}
+		//printf("pas bonne commande");
+		free(full_paths);
+		i++;
 	}
 	return(0);
 }

f3ad33cd664dcba9960e3eeb94373e71896ee663
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-03-29 14:01:55 +0100

    1

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 11:41:52 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/03/28 11:43:49 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/03/29 11:27:37 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 void	cmd_error(void)
 {
 	perror(EM);
+	exit(EXIT_FAILURE);
+}
+
+void	exit_error(bool boolean)
+{
+	if (bool == true)
+		exit(EXIT_SUCCES);
 	exit(EXIT_FAILURE);
 }
